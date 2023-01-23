246f4cdedc62d4c620ae6e93a3744dd91d4f6fbe
ficazam <felipeicaza@gmail.com>
2023-01-23 09:48:49 -0500

    frontend fixes

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,21 +4,31 @@ import { MailInput, PasswordInput } from "../components";
 export const Home = () => {
   const [mail, setMail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
+  const authHandler = () => {
+    console.log(mail);
+  };
+
   return (
     <div className="h-screen w-full flex items-center justify-center">
       Welcome to the Webtronic Labs POC! Please sign in:
-      <MailInput
-        mail={mail}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setMail(e.target.value)
-        }
-      />
-      <PasswordInput
-        pass={password}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setPassword(e.target.value)
-        }
-      />
+      <form className="flex-col justify-center">
+        <MailInput
+          mail={mail}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setMail(e.target.value)
+          }
+        />
+        <PasswordInput
+          pass={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+        />
+        <button type="submit" onClick={authHandler}>
+          Sign In
+        </button>
+      </form>
     </div>
   );
 };
